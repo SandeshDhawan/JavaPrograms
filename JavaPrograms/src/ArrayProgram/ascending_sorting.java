776ef099f2300fb411b9ba0b5f372554beda6be8
SandeshDhawan <sdhawan12494@gmail.com>
2020-06-18 14:46:26 +0530

    ascending sorting

---
+++ JavaPrograms/src/ArrayProgram/ascending_sorting.java
@@ -0,0 +1,40 @@
+package ArrayProgram;
+
+import java.util.Scanner;
+
+public class ascending_sorting {
+	public static void main(String[] args) {
+		int n, temp;
+		Scanner k = new Scanner(System.in);
+
+		System.out.println("How Many Number you want in Array");
+		n = k.nextInt();
+
+		int a[] = new int[n];
+		System.out.println("Enter " + n + " Number in Array");
+		for (int i = 0; i < n; i++)
+			a[i] = k.nextInt();
+
+		System.out.println("Number in Array are");
+		for (int i = 0; i < n; i++)
+			System.out.print(a[i] + " ");
+		System.out.println();
+
+		for (int j = 0; j < n; j++) {
+			for (int i = 0; i < n - 1; i++) {
+				if (a[i] > a[i + 1]) {
+					temp = a[i];
+					a[i] = a[i + 1];
+					a[i + 1] = temp;
+				}
+			}
+		}
+
+		System.out.println("Array in Ascending Order is=");
+		for (int i = 0; i < n; i++)
+			System.out.print(a[i] + " ");
+		System.out.println();
+
+	}
+
+}
