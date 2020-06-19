c79b57d397e82006b0441c085126564bf2aa36a3
SandeshDhawan <sdhawan12494@gmail.com>
2020-06-19 17:54:25 +0530

    Code Format

--- JavaPrograms/src/ForLoopProgram/check_factorial_of_number.java
+++ JavaPrograms/src/ForLoopProgram/check_factorial_of_number.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class check_factorial_of_number {
 	public static void main(String[] args) {
-		int n,fact=1;
-		Scanner k=new Scanner(System.in);
+		int n, fact = 1;
+		Scanner k = new Scanner(System.in);
 		System.out.println("Enter Any Number");
-		n=k.nextInt();
-		
-		for(int i=1;i<=n;i++)
-			fact=fact*i;
-		
-		System.out.println("Factorial of number "+n+" is ="+fact);
-		
+		n = k.nextInt();
+
+		for (int i = 1; i <= n; i++)
+			fact = fact * i;
+
+		System.out.println("Factorial of number " + n + " is =" + fact);
+
 	}
 
 }
