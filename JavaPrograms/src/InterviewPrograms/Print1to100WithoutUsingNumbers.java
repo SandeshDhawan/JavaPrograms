74fd5932f78b37cca53de1bcfc663612f880e306
Sandesh <sdhawan12494@gmail.com>
2021-09-23 10:09:45 +0530

    Interview Programs

---
+++ JavaPrograms/src/InterviewPrograms/Print1to100WithoutUsingNumbers.java
@@ -0,0 +1,16 @@
+package InterviewPrograms;
+
+public class Print1to100WithoutUsingNumbers {
+	public static void main(String[] args) {
+		int one = 'A' / 'A'; // 1
+		String str = "..........";
+
+//		for (int i=one;i<=str.length()*str.length();i++)
+//			System.out.println(i); 
+
+		for (int i = one; i <= 'd'; i++)
+			System.out.println(i);
+
+	}
+
+}
